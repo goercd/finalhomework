9e3a98840ac06104b9a0682ca3bb58f88d990db9
goercd <46823854+goercd@users.noreply.github.com>
2019-04-14 19:09:21 +0300

    Create Cart.js

---
+++ src/components/Cart.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Product from './Product'
+
+
+/* вывод списка супергероев */
+
+
+const Cart  = ({ products, total, onCheckoutClicked }) => {
+  const hasProducts = products.length > 0
+  const nodes = hasProducts ? (
+    products.map(product =>
+      <Product
+        title={product.title}
+        key={product.id}
+      />
+    )
+  ) : (
+    <em></em>
+  )
+
+  return (
+
+    <div>
+      <h3>Список выбранных супергероев</h3>
+      
+      <div>{nodes}</div>
+      
+
+      <button onClick={onCheckoutClicked}
+        disabled={hasProducts ? '' : 'disabled'}>
+        Checkout
+      </button>
+    </div>
+  )
+}
+
+
+
+Cart.propTypes = {
+  products: PropTypes.array,
+  total: PropTypes.string,
+  onCheckoutClicked: PropTypes.func
+}
+
+export default Cart
